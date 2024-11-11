c4adcf90db68928581782ed998dbc8b56eca4e1d
G.A. Dilsara Thiranjaya <dilsarathiranjaya3@gmail.com>
2024-11-11 15:38:01 +0530

    Updated

--- assets/js/case-1.js
+++ assets/js/case-1.js
@@ -1,4 +1,23 @@
-setInterval(() => {
-    const lastElement = $('.walking-man-container div:last-child');
-    $('.walking-man-container').prepend(lastElement);
-}, 2500);
+let interval;
+
+function startAnimation() {
+    if (!interval) {
+        interval = setInterval(() => {
+            const lastElement = $('.walking-man-container div:last-child');
+            $('.walking-man-container').prepend(lastElement);
+        }, 2500);
+    }
+}
+
+function stopAnimation() {
+    clearInterval(interval);
+    interval = null;
+}
+
+$('#start').click(() => {
+    startAnimation();
+});
+
+$('#stop').click(() => {
+    stopAnimation();
+});
--- assets/js/case-2.js
+++ assets/js/case-2.js
@@ -1,11 +1,31 @@
 const boxes = $('.box').toArray();
 let colors = boxes.map(box => $(box).css('background-color'));
-    
-setInterval(() => {
-    const lastColor = colors.pop();
-    colors.unshift(lastColor);
+let interval;
 
-    boxes.forEach((box, index) => {
-        $(box).css('background-color', colors[index]);
-    });   
-}, 500);
+function startAnimation() {
+    if (!interval) {
+        interval = setInterval(() => {
+            const lastColor = colors.pop();
+            colors.unshift(lastColor);
+        
+            boxes.forEach((box, index) => {
+                $(box).css('background-color', colors[index]);
+            });   
+        }, 500);
+    }
+}
+
+function stopAnimation() {
+    clearInterval(interval);
+    interval = null;
+}
+
+
+
+$('#start').click(() => {
+    startAnimation();
+});
+
+$('#stop').click(() => {
+    stopAnimation();
+});
